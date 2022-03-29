11ab67430cb0bbf28f4ac2060f15bc90152996ba
LuisEncastin <luisencastin@gmail>
2022-03-28 23:55:36 -0600

    Desarrollo de rutas con react router. Clase 11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
+import App from './routes/App.jsx';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
-
-// Hola
